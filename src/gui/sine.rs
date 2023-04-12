efd7f2925fc3e9468214bbb0556f8c18634d5a0a
Jeffrey Rosenbluth <jeffrey.rosenbluth@gmail.com>
2023-04-12 12:39:38 -0400

    sine

---
+++ src/gui/sine.rs
@@ -0,0 +1,72 @@
+use crate::gui::lslider::LSlider;
+use crate::Message::{self, *};
+use iced::widget::{Column, Rule};
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Sine {
+    pub xfreq: f32,
+    pub yfreq: f32,
+    pub xexp: f32,
+    pub yexp: f32,
+}
+
+impl<'a> Sine {
+    pub fn new(xfreq: f32, yfreq: f32, xexp: f32, yexp: f32) -> Self {
+        Self {
+            xfreq,
+            yfreq,
+            xexp,
+            yexp,
+        }
+    }
+    pub fn show(&self) -> Column<'a, Message> {
+        Column::new()
+            .push(Rule::horizontal(10))
+            .push("Sine Noise")
+            .push(
+                LSlider::new(
+                    "Sine x Frequency".to_string(),
+                    self.xfreq,
+                    0.1..=4.0,
+                    0.1,
+                    XFreq,
+                    Draw,
+                )
+                .decimals(1),
+            )
+            .push(
+                LSlider::new(
+                    "Sine y Frequency".to_string(),
+                    self.yfreq,
+                    0.1..=4.0,
+                    0.1,
+                    YFreq,
+                    Draw,
+                )
+                .decimals(1),
+            )
+            .push(
+                LSlider::new(
+                    "Sine x Exponent".to_string(),
+                    self.xexp,
+                    1.0..=4.0,
+                    1.0,
+                    XExp,
+                    Draw,
+                )
+                .decimals(0),
+            )
+            .push(
+                LSlider::new(
+                    "Sine y Exponent".to_string(),
+                    self.yexp,
+                    1.0..=4.0,
+                    1.0,
+                    YExp,
+                    Draw,
+                )
+                .decimals(0),
+            )
+            .spacing(15)
+    }
+}
